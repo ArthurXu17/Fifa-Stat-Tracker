d2f66faf09d0cb70cdccf9d33c1695cddb2fae18
Arthur Xu <arthuryangxu100@gmail.com>
2023-08-09 21:32:30 -0400

    Add Player Stats to View Team Stats Page

--- fifa_stat_tracker/tracker/models/player.py
+++ fifa_stat_tracker/tracker/models/player.py
@@ -9,3 +9,24 @@ class Player(models.Model):
     
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name} ({self.number})"
+    
+    def num_shots(self):
+        return self.shots.count() + self.goals.count()
+    
+    def num_blocked_shots(self):
+        return self.shots.filter(blocked_by_player=True).count()
+    
+    def num_shots_on_target(self):
+        return self.shots.filter(on_target=True).count() + self.goals.count()
+    
+    def num_impressive_goals(self):
+        return self.goals.filter(is_impressive_goal=True).count()
+    
+    def num_not_impressive_goals(self):
+        return self.goals.filter(is_impressive_goal=False).count()
+    
+    def num_impressive_assists(self):
+        return self.goals_assisted_on.filter(is_impressive_assist=True).count()
+   
+    def num_not_impressive_assists(self):
+        return self.goals_assisted_on.filter(is_impressive_assist=False).count()
