d3a25e94ebe339a6e36f5c9ac76371bc4cc157d1
Arthur Xu <arthuryangxu100@gmail.com>
2023-08-09 23:41:51 -0400

    Add More Descriptive Game Logs

--- fifa_stat_tracker/tracker/models/goal.py
+++ fifa_stat_tracker/tracker/models/goal.py
@@ -12,7 +12,16 @@ class Goal(AbstractShot):
     is_impressive_goal = models.BooleanField()
     
     def display_information(self):
-        return f"Goal by {self.player.first_name} {self.player.last_name} at {self.minute}'"
+        header_prefix = "Header" if self.body_part == "Head" else ""
+        goal = "GOOOAL" if self.is_impressive_goal else "Goal"
+        goal += f" by {self.player.first_name} {self.player.last_name}"
+        corner_suffix = f"generated from corner at {self.corner.minute}'" if self.corner is not None else ""
+        if self.assist:
+            assist = "assisted" if self.is_impressive_assist else "uhssisted"
+            goal = f"{header_prefix} {goal} {assist} by {self.assist.first_name} {self.assist.last_name}"
+        else:
+            goal = f"Unassisted {header_prefix} {goal}"
+        return f"{goal} at {self.minute}' {corner_suffix}"
     
     class Meta:
         default_related_name = 'goals'
--- fifa_stat_tracker/tracker/models/shot.py
+++ fifa_stat_tracker/tracker/models/shot.py
@@ -5,7 +5,11 @@ from .abstract_shot import AbstractShot
 class Shot(AbstractShot):
     
     def display_information(self):
-        return f"Shot by {self.player.first_name} {self.player.last_name} at {self.minute}'"
+        verb = "Shot" if self.body_part == "Foot" else "Header"
+        on_target = "on target" if self.on_target else "off target"
+        corner_suffix = f"generated from corner at {self.corner.minute}'" if self.corner is not None else ""
+        blocked = "Blocked" if self.blocked_by_player else ""
+        return f"{blocked} {verb} {on_target} by {self.player.first_name} {self.player.last_name} at {self.minute}' {corner_suffix}"
     
     class Meta:
         default_related_name = 'shots'
