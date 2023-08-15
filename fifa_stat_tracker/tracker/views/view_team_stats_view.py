0f99371fd4d413d9343042990769a7ff4063e811
Arthur Xu <arthuryangxu100@gmail.com>
2023-08-15 14:28:30 -0400

    Compute Averages For W/D/L Only If Corresponding Games Exist

--- fifa_stat_tracker/tracker/views/view_team_stats_view.py
+++ fifa_stat_tracker/tracker/views/view_team_stats_view.py
@@ -13,22 +13,30 @@ class ViewTeamStatsView(DetailView):
         stats = [('num_shots', 'Shots'), ('num_blocked_shots', 'Blocked Shots'), ('num_shots_on_target', 'Shots on Target'), 
                  ('num_goals', 'Goals'), ('num_assists', 'Assists'), ('num_corners', 'Corners')]
         stat_rows = []
-        if team.matches.count() > 0:
-            for stat, formatted_stat in stats:
+        for stat, formatted_stat in stats:
+            win_mean = win_median = draw_mean = draw_median = loss_mean = loss_median = mean = median = "N/A"
+            if team.matches.filter(result='W').exists():
                 win_mean, win_median = team.average_stat_by_result(stat, result='W')
+                win_mean = f"{win_mean:.2f}"
+            if team.matches.filter(result='D').exists():
                 draw_mean, draw_median = team.average_stat_by_result(stat, result='D')
+                draw_mean = f"{draw_mean:.2f}"
+            if team.matches.filter(result='L').exists():
                 loss_mean, loss_median = team.average_stat_by_result(stat, result='L')
+                loss_mean = f"{loss_mean:.2f}"
+            if team.matches.count() > 0:
                 mean, median = team.average_stat_by_result(stat)
-                stat_rows.append({
-                    'stat': formatted_stat,
-                    'win_mean': f"{win_mean:.2f}",
-                    'win_median': win_median,
-                    'draw_mean': f"{draw_mean:.2f}",
-                    'draw_median': draw_median,
-                    'loss_mean': f"{loss_mean:.2f}",
-                    'loss_median': loss_median,
-                    'overall_mean': f"{mean:.2f}",
-                    'overall_median': median
-                })
+                mean = f"{mean:.2f}"
+            stat_rows.append({
+                'stat': formatted_stat,
+                'win_mean': win_mean,
+                'win_median': win_median,
+                'draw_mean': draw_mean,
+                'draw_median': draw_median,
+                'loss_mean': loss_mean,
+                'loss_median': loss_median,
+                'overall_mean': mean,
+                'overall_median': median
+            })
         context.update({'stat_rows': stat_rows})
         return context
