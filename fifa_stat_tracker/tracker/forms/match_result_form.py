ca890e00569370a4c03b77827e973e262c32d3e6
Arthur Xu <arthuryangxu100@gmail.com>
2023-08-17 10:54:19 -0400

    Fix Crispy Forms By Adding Submit Object to FormHelper

--- fifa_stat_tracker/tracker/forms/match_result_form.py
+++ fifa_stat_tracker/tracker/forms/match_result_form.py
@@ -1,6 +1,14 @@
 from django import forms
+from crispy_forms.layout import Submit
+from crispy_forms.helper import FormHelper
 
 class MatchResultForm(forms.Form):
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('end_game', 'End Game'))
+        
     result = forms.ChoiceField(choices=[
         ('W', 'Win'),
         ('D', 'Draw'),
