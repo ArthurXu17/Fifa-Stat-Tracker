50cad11938d19ae6f13313fe0cd54f7a6890053e
Arthur Xu <arthuryangxu100@gmail.com>
2023-09-09 15:43:26 -0400

    Add Home Page

--- fifa_stat_tracker/tracker/urls.py
+++ fifa_stat_tracker/tracker/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from .views.home_page_view import HomePageView
 from .views.view_stats_view import ViewStatsView
 from .views.play_game_view import play_game_view
 from .views.initialize_team_view import InitializeTeamView
@@ -10,6 +11,7 @@ from .views.top_players_view import TopPlayersView
 from .views.user_stats_view import UserStatsView
 
 urlpatterns = [
+    path("", HomePageView.as_view(), name="home_page"),
     path("initialize_team", InitializeTeamView.as_view(), name="initialize_team"),
     path("play_game/<int:match_id>", play_game_view, name="play_game"),
     path("start_game", StartGameView.as_view(), name="start_game"),
